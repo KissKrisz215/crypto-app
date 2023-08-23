b20b17a45ae72c3cef291e5f9867d13590343469
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-08-23 23:31:53 +0200

    Fixed GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,22 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from 'styled-components';
 import { CoinsList, Portfolio, Coin } from "./pages/index";
 import { GlobalStyle } from "./styles/GlobalStyle";
+import { Navbar } from './components';
 import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-      <ThemeProvider>
-      <GlobalStyle>
+      <ThemeProvider theme={{}}>
+      <GlobalStyle />
+      <Navbar />
         <Routes>
           <Route path="/" element={<CoinsList />} />
           <Route path="/coins/:coinId" element={<Coin />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-        </GlobalStyle>
         </ThemeProvider>
       </div>
     );
