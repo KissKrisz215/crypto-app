1d50abe8635f0e0f27f603f558a96eecc6b9b647
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-10-11 20:22:15 +0200

    Converted LoadingBar to hooks

--- src/components/LoadingAnimations/LoadingBar/LoadingBar.jsx
+++ src/components/LoadingAnimations/LoadingBar/LoadingBar.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { Wrapper, InnerWrapper } from "./LoadingBar.styles";
 
-export default class LoadingBar extends React.Component {
-  render() {
-    const { width, height } = this.props;
-    return (
-      <Wrapper width={width}>
-        <InnerWrapper height={height}></InnerWrapper>
-      </Wrapper>
-    );
-  }
-}
+const LoadingBar = ({ width, height }) => (
+  <Wrapper width={width}>
+    <InnerWrapper height={height}></InnerWrapper>
+  </Wrapper>
+);
+
+export default LoadingBar;
