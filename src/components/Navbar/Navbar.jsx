c7f2d6454d42e839a6a716d42e2465d9768672fe
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-10-11 20:26:18 +0200

    Converted Navbar to hooks

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,35 +8,35 @@ import {
 import { SearchBar, CurrencyToggle, ThemeToggle } from "../index";
 import SubHeader from "../SubHeader/SubHeader";
 
-export default class Navbar extends React.Component {
-  render() {
-    return (
-      <Wrapper>
-        <SubHeader activeCurrency={this.props.activeCurrency} />
-        <NavbarContainer>
-          <Container>
-            <NavbarLink page="home" active={this.props.active} to="/">
-              Coins
-            </NavbarLink>
-            <NavbarLink
-              page="portfolio"
-              active={this.props.active}
-              to="/portfolio"
-            >
-              Portfolio
-            </NavbarLink>
-          </Container>
-          <Container gap="1rem">
-            <SearchBar />
-            <CurrencyToggle
-              activeCurrency={this.props.activeCurrency}
-              handleActiveCurrency={this.props.handleActiveCurrency}
-              currencies={this.props.currencies}
-            />
-            <ThemeToggle handleChangeTheme={this.props.handleChangeTheme} />
-          </Container>
-        </NavbarContainer>
-      </Wrapper>
-    );
-  }
-}
+const Navbar = ({
+  activeCurrency,
+  active,
+  handleActiveCurrency,
+  currencies,
+  handleChangeTheme,
+}) => (
+  <Wrapper>
+    <SubHeader activeCurrency={activeCurrency} />
+    <NavbarContainer>
+      <Container>
+        <NavbarLink page="home" active={active} to="/">
+          Coins
+        </NavbarLink>
+        <NavbarLink page="portfolio" active={active} to="/portfolio">
+          Portfolio
+        </NavbarLink>
+      </Container>
+      <Container gap="1rem">
+        <SearchBar />
+        <CurrencyToggle
+          activeCurrency={activeCurrency}
+          handleActiveCurrency={handleActiveCurrency}
+          currencies={currencies}
+        />
+        <ThemeToggle handleChangeTheme={handleChangeTheme} />
+      </Container>
+    </NavbarContainer>
+  </Wrapper>
+);
+
+export default Navbar;
