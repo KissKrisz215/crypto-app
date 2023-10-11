c22ba4141f67cd35918ca2ba7d89ee5823d7a884
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-10-11 20:09:28 +0200

    Converted CoinPercentage Component to hooks

--- src/components/CoinPercentage/CoinPercentage.jsx
+++ src/components/CoinPercentage/CoinPercentage.jsx
@@ -8,18 +8,15 @@ import {
 import Icons from "../../assets/index";
 import { addCommas } from "../../utils/formatPrices";
 
-export default class CoinPercentage extends React.Component {
-  render() {
-    const { data } = this.props;
-    return (
-      <Container>
-        <Wrapper>
-          <ArrowLogo coinChange={data} src={Icons.ArrowIcon} />
-          <CoinData coinChange={data}>
-            {Math.abs(addCommas(data)) + "%"}
-          </CoinData>
-        </Wrapper>
-      </Container>
-    );
-  }
-}
+const CoinPercentage = ({ data }) => {
+  return (
+    <Container>
+      <Wrapper>
+        <ArrowLogo coinChange={data} src={Icons.ArrowIcon} />
+        <CoinData coinChange={data}>{Math.abs(addCommas(data)) + "%"}</CoinData>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default CoinPercentage;
