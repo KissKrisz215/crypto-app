f65eb314bb4fdd96e8b5000018ee401067aa636e
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-10-11 20:27:31 +0200

    Converted PercentageBar to hooks

--- src/components/PercentageBar/PercentageBar.jsx
+++ src/components/PercentageBar/PercentageBar.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { Container, Percentage } from "./PercentageBar.styles";
 
-export default class PercentageBar extends React.Component {
-  render() {
-    const { percentage } = this.props;
-    return (
-      <Container>
-        <Percentage percentage={percentage} />
-      </Container>
-    );
-  }
-}
+const PercentageBar = ({ percentage }) => (
+  <Container>
+    <Percentage percentage={percentage} />
+  </Container>
+);
+
+export default PercentageBar;
