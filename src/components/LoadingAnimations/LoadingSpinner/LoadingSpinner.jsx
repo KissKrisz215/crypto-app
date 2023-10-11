e7830f91c4866c6cb6e6809ddbd4da6881427923
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-10-11 20:23:04 +0200

    Converted LoadingSpinner to hooks

--- src/components/LoadingAnimations/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingAnimations/LoadingSpinner/LoadingSpinner.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import { Container, LoadingBar } from "./LoadingSpinner.styles";
 
-export default class LoadingSpinner extends React.Component {
-  render() {
-    const { width, height, border, color, borderColor } = this.props;
-    return (
-      <Container>
-        <LoadingBar
-          height={height}
-          width={width}
-          border={border}
-          color={color}
-          borderColor={borderColor}
-        />
-      </Container>
-    );
-  }
-}
+const LoadingSpinner = ({ width, height, border, color, borderColor }) => (
+  <Container>
+    <LoadingBar
+      height={height}
+      width={width}
+      border={border}
+      color={color}
+      borderColor={borderColor}
+    />
+  </Container>
+);
+
+export default LoadingSpinner;
